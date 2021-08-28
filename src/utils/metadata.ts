23d8affd79928bb5340bdcc2f19b67be08f8e711
thasarito <thasarit.pysr@gmail.com>
2021-08-28 16:51:02 +0700

    add book title as metadata

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -2,9 +2,10 @@ const metadata = {
 	year: 'ปีที่ยืม',
 	month: 'เดือนที่ยืม',
 	day: 'วันที่ยืม',
-	faculty: 'คณะ',
+	faculty: 'ประเภทบุคลากร',
 	library: 'ห้องสมุด',
-	period: 'ช่วงเวลาของวัน'
+	period: 'ช่วงเวลาของวัน',
+	title: 'หนังสือ'
 };
 
 export type TMetadata = typeof metadata;
