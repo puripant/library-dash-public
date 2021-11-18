4939a761a063d72d924522b9d3832d5bc7070685
thasarito <thasarit.pysr@gmail.com>
2021-11-18 10:31:52 +0700

    add label box for quantitative data

--- Frontend/src/components/labels.svelte
+++ Frontend/src/components/labels.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { range } from 'd3-array';
 	import type { TColor } from 'src/utils/barcolors';
 
 	export let w: number,
@@ -7,17 +8,16 @@
 		color: TColor;
 
 	let show = false;
-
-	console.log('color', color.label);
 </script>
 
-<g
-	transform={`translate(${w - margin.right / 2 - (show ? w / 2 : 0)}, ${margin.top / 2})`}
-	class="transition-all"
-	on:mouseenter={() => (show = true)}
-	on:mouseleave={() => (show = false)}
->
-	{#if color.label.type === 'qualitative'}
+{#if color.label.type === 'qualitative'}
+	<g
+		transform={`translate(${w - margin.right / 2 - (show ? w / 2 : -w * 0.3)}, ${margin.top / 2})`}
+		opacity={0.95}
+		class="transition-all"
+		on:mouseenter={() => (show = true)}
+		on:mouseleave={() => (show = false)}
+	>
 		<rect width={w} height={h} fill="white" stroke="gainsboro" />
 		{#each Object.entries(color.label.val) as [dim, color], i}
 			<g transform={`translate(5, ${15 * i + 5})`}>
@@ -27,5 +27,19 @@
 				</text>
 			</g>
 		{/each}
-	{/if}
-</g>
+	</g>
+{/if}
+{#if color.label.type === 'quantitative'}
+	<g transform={`translate(${w - margin.right / 2 + 25}, ${margin.top / 2})`} opacity={0.95}>
+		<rect width={50} height={h / 2} fill="white" stroke="gainsboro" />
+		<g transform={`translate(5, 5)`} font-size={10}>
+			{#each range(...color.label.val, (color.label.val[1] - color.label.val[0]) / ((h / 2 - 10) / 2)).reverse() as value, i}
+				<g transform={`translate(0, ${2 * i})`}>
+					<rect width={10} height={2} fill={color(value)} />
+				</g>
+			{/each}
+			<text dominant-baseline="middle" x={15} y={5}> {color.label.val[1]} </text>
+			<text dominant-baseline="middle" x={15} y={h / 2 - 15}> {color.label.val[0]} </text>
+		</g>
+	</g>
+{/if}
